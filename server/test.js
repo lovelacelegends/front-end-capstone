5676ee40167a7534dd7114a8e01ceb4b5b4f648f
Timothy Nguyen <shimoshixp@gmail.com>
2021-02-26 18:30:12 -0800

    Fix linter errors in test.js

--- server/test.js
+++ server/test.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 function sum(a, b) {
   return a + b;
 }
 
-describe ('test', () => {
+describe('test', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-})
+});
