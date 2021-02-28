fdb9aa9835607d91efdf46911f44341561e44cf0
larakhdavies <lara.kh.davies@gmail.com>
2021-02-27 16:41:18 -1000

    add components to overview folder

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import ProductInfo from './ProductInfo';
+import ProductOverview from './ProductOverview';
+import StyleSelector from './StyleSelector';
+import ImageGallery from './ImageGallery';
 
-const Overview = () => (
-  <div>Overview</div>
-);
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <ImageGallery />
+        <ProductInfo />
+        <StyleSelector />
+        <ProductOverview />
+      </div>
+    );
+  }
+}
 
 export default Overview;
---
+++ client/src/components/overview/ProductOverview.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProductOverview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        ProductOverview
+      </div>
+    );
+  }
+}
+
+export default ProductOverview;
