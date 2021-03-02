b8f0fa2b11f247ad11e08aa87fe99bcfbe4e61dd
Timothy Nguyen <shimoshixp@gmail.com>
2021-03-01 18:12:54 -0800

    Add basic skeleton for questions/answers and ratings/reviews

--- client/src/components/questionsAndAnswers/QuestionsAndAnswers.jsx
+++ client/src/components/questionsAndAnswers/QuestionsAndAnswers.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const QuestionsAndAnswers = () => (
-  <div>QuestionsAndAnswers</div>
+  <div className="questions-and-answers">QuestionsAndAnswers</div>
 );
 
 export default QuestionsAndAnswers;
