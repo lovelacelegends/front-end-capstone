27bbb497a7bd89a18b09afed947f7076e304a546
larakhdavies <lara.kh.davies@gmail.com>
2021-03-02 19:32:10 -1000

    added drop down box with sizes and will change state when different size is selected

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -4,14 +4,27 @@ import SizeSelector from './SizeSelector';
 class AddToCart extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentSku: {},
+    };
+    this.updateSkuInState = this.updateSkuInState.bind(this);
+  }
+
+  updateSkuInState(sku) {
+    this.setState({ currentSku: sku });
   }
 
   render() {
     const { styles, currentStyle } = this.props;
+    const { currentSku } = this.state;
     return (
       <div className="add-to-cart">
-        <SizeSelector styles={styles} currentStyle={currentStyle} />
+        <SizeSelector
+          styles={styles}
+          currentStyle={currentStyle}
+          currentSku={currentSku}
+          updateSkuInState={this.updateSkuInState}
+        />
       </div>
     );
   }
--- client/src/components/overview/SizeSelector.jsx
+++ client/src/components/overview/SizeSelector.jsx
@@ -5,25 +5,37 @@ class SizeSelector extends React.Component {
     super(props);
     this.state = {
     };
+    this.updateSku = this.updateSku.bind(this);
+  }
+
+  updateSku(e) {
+    e.preventDefault();
+    const index = e.target.selectedIndex;
+    let selectedSku = e.target.childNodes[index].id;
+    this.props.updateSkuInState(selectedSku);
   }
 
   render() {
     const { styles, currentStyle } = this.props;
     const objOfSkus = styles.results[currentStyle].skus;
 
-    let arrayOfSkus = [];
+    const arrayOfSkus = [];
     Object.keys(objOfSkus).forEach((key) => {
       arrayOfSkus.push(key);
     });
 
     return (
       <div className="size-selector">
-        <select>
+        <select onChange={this.updateSku}>
           <option>SELECT SIZE</option>
           {arrayOfSkus.map((sku) => {
-            if (objOfSkus[sku]["quantity"] !== 0) {
-              const sizetoDisplay = objOfSkus[sku]["size"];
-              return <option>{sizetoDisplay}</option>;
+            if (objOfSkus[sku].quantity !== 0) {
+              const sizetoDisplay = objOfSkus[sku].size;
+              return (
+                <option id={sku}>
+                {sizetoDisplay}
+                </option>
+              );
             }
             return <option disabled>OUT OF STOCK</option>;
           })}
