15a258e595388a96f51dc1fa9f0cc968d408476d
Timothy Nguyen <shimoshixp@gmail.com>
2021-03-06 02:41:47 -0800

    Refactor server/index.js to be cleaner

--- client/src/components/ratingsAndReviews/ReviewModal.jsx
+++ client/src/components/ratingsAndReviews/ReviewModal.jsx
@@ -5,11 +5,31 @@ class ReviewModal extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      stars: 0,
+      recommend: '',
+      size: null,
+      width: null,
+      comfort: null,
+      quality: null,
+      length: null,
+      fit: null,
+      summary: '',
+      textarea: '',
+    };
   }
 
   render() {
-    const { showModal, toggleModal } = this.props;
+    const {
+      showModal,
+      toggleModal,
+    } = this.props;
+
+    const {
+      stars,
+      summary,
+      textarea,
+    } = this.state;
 
     if (!showModal) {
       return null;
@@ -19,7 +39,7 @@ class ReviewModal extends React.Component {
       <form className="review-modal">
         <h3>Write Your Review</h3>
         <h6>About the Product</h6>
-        <div className="stars" style={{ '--rating': 0 }} />
+        <div className="stars" style={{ '--rating': stars }} />
         <br />
         <span>Do you recommend this product?</span>
         <label>
@@ -31,9 +51,149 @@ class ReviewModal extends React.Component {
           No
         </label>
         <br />
-        <input type="text" placeholder="Example: Best purchase ever!" />
+        <span>Size</span>
+        <label>
+          <input type="radio" value="1" />
+          A size too small
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          1/2 a size too small
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          Perfect
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          1/2 a size too big
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          A size too wide
+        </label>
         <br />
-        <textarea type="text" placeholder="Why did you like the product ornot?" />
+        <span>Width</span>
+        <label>
+          <input type="radio" value="1" />
+          Too narrow
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          Slightly narrow
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          Perfect
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          Slightly wide
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          Too wide
+        </label>
+        <br />
+        <span>Comfort</span>
+        <label>
+          <input type="radio" value="1" />
+          Uncomfortable
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          Slightly uncomfortable
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          Ok
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          Comfortable
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          Perfect
+        </label>
+        <br />
+        <span>Quality</span>
+        <label>
+          <input type="radio" value="1" />
+          Poor
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          Below average
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          What I expected
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          Pretty great
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          Perfect
+        </label>
+        <br />
+        <span>Length</span>
+        <label>
+          <input type="radio" value="1" />
+          Runs short
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          Runs slightly short
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          Perfect
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          Runs slightly long
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          Runs long
+        </label>
+        <br />
+        <span>Fit</span>
+        <label>
+          <input type="radio" value="1" />
+          Runs tight
+        </label>
+        <label>
+          <input type="radio" value="2" />
+          Runs slightly tight
+        </label>
+        <label>
+          <input type="radio" value="3" />
+          Perfect
+        </label>
+        <label>
+          <input type="radio" value="4" />
+          Runs slightly long
+        </label>
+        <label>
+          <input type="radio" value="5" />
+          Runs long
+        </label>
+        <br />
+        <input
+          type="text"
+          placeholder="Example: Best purchase ever!"
+          value={summary}
+        />
+        <br />
+        <textarea
+          type="text"
+          placeholder="Why did you like the product ornot?"
+          value={textarea}
+        />
         <br />
         <input type="submit" />
         <button type="button" onClick={toggleModal}>Close</button>
--- server/index.js
+++ server/index.js
@@ -7,19 +7,16 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// app.get('/', (req, res) => {
-// });
-
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
 
-  const p1 = atlier.getProductById(id);
-  const p2 = atlier.getProductStylesById(id);
-  const p3 = atlier.getRelatedProductsById(id);
-  const p4 = atlier.getReviewsById(id);
-  const p5 = atlier.getMetaReviewsById(id);
-
-  Promise.all([p1, p2, p3, p4, p5])
+  Promise.all([
+    atlier.getProductById(id),
+    atlier.getProductStylesById(id),
+    atlier.getRelatedProductsById(id),
+    atlier.getReviewsById(id),
+    atlier.getMetaReviewsById(id),
+  ])
     .then((data) => {
       res.status(201).send(data);
     })
@@ -31,21 +28,21 @@ app.get('/products/:id', (req, res) => {
 app.get('/related/:id', (req, res) => {
   const { id } = req.params;
 
-  const p1 = atlier.getProductById(id);
-  const p2 = atlier.getProductStylesById(id);
-  const p3 = atlier.getMetaReviewsById(id);
-
-  Promise.all([p1, p2, p3])
+  Promise.all([
+    atlier.getProductById(id),
+    atlier.getProductStylesById(id),
+    atlier.getMetaReviewsById(id),
+  ])
     .then((data) => {
-      const relatedData = {};
-
-      relatedData.id = data[0].id;
-      relatedData.name = data[0].name;
-      relatedData.category = data[0].category;
-      relatedData.price = data[0].default_price;
-      relatedData.features = data[0].features;
-      relatedData.url = data[1].results[0].photos[0].url;
-      relatedData.ratings = data[2].ratings;
+      const relatedData = {
+        id: data[0].id,
+        name: data[0].name,
+        category: data[0].category,
+        price: data[0].default_price,
+        features: data[0].features,
+        url: data[1].results[0].photos[0].url,
+        ratings: data[2].ratings,
+      };
 
       res.status(201).send(relatedData);
     })
