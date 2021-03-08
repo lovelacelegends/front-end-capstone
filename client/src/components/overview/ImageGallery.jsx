16b58926f842644830870a064d3ec8db19ef8a82
larakhdavies <lara.kh.davies@gmail.com>
2021-03-07 19:37:28 -1000

    css changes

--- client/src/components/overview/ImageGallery.jsx
+++ client/src/components/overview/ImageGallery.jsx
@@ -35,17 +35,18 @@ class ImageGallery extends React.Component {
   }
 
   render() {
-    const { styles, currentStyle, handleExpandedView } = this.props;
+    const { styles, currentStyle, handleExpandedView, displayGroupedExtras } = this.props;
     const { mainThumbNailIndex } = this.state;
     return (
-      <div id="image-gallery"
-      style={{ backgroundImage: `url(${styles.results[currentStyle].photos[mainThumbNailIndex].thumbnail_url})`}}
-      >
-        <Thumbnails styles={styles} currentStyle={currentStyle} thumbnailClick={this.thumbnailClick}/>
-        <div className="both-arrows">
-          <BsArrowLeftShort className="left-arrow" onClick={this.leftArrowClick} />
-          <BsArrowRightShort className="right-arrow" onClick={this.rightArrowClick} />
-        </div>
+      <div id="image-gallery">
+        <img id="large-img" className={displayGroupedExtras ? null : "large-img-expanded"} src={styles.results[currentStyle].photos[mainThumbNailIndex].url} alt="large image"></img>
+        <Thumbnails
+          styles={styles}
+          currentStyle={currentStyle}
+          thumbnailClick={this.thumbnailClick}
+        />
+        <BsArrowLeftShort className="left-arrow" onClick={this.leftArrowClick} />
+        <BsArrowRightShort className="right-arrow" onClick={this.rightArrowClick} />
         <div className="expand-button"><AiOutlineExpand onClick={handleExpandedView} /></div>
       </div>
     );
@@ -53,3 +54,61 @@ class ImageGallery extends React.Component {
 }
 
 export default ImageGallery;
+
+/*
+  componentDidMount() {
+    this.magnify('large-img', 3);
+  }
+
+  magnify(imgID, zoom) {
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "img-magnifier-glass");
+
+    img.parentElement.insertBefore(glass, img);
+
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+
+    function moveMagnifier(e) {
+      var pos, x, y;
+
+      e.preventDefault();
+      pos = getCursorPos(e);
+      x = pos.x;
+      y = pos.y;
+      if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+      if (x < w / zoom) {x = w / zoom;}
+      if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+      if (y < h / zoom) {y = h / zoom;}
+
+      glass.style.left = (x - w) + "px";
+      glass.style.top = (y - h) + "px";
+
+      glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+    }
+
+    function getCursorPos(e) {
+      var a, x = 0, y = 0;
+      e = e || window.event;
+
+      a = img.getBoundingClientRect();
+
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+    }
+  }
+
+*/
