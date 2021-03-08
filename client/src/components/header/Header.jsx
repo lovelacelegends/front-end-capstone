8b71f421bb6ee0c1d7c0b9cd71f3d9fd6a3b9782
Jack <jackpronske@yahoo.com>
2021-03-07 18:33:43 -0800

    --implemented the logo in the heading with some basic styling

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import SaleModal from './SaleModal';
 
-const Header = () => (
-  <div>Header</div>
-);
+// const Header = () => (
+//   <div>Header</div>
+// );
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSale: true,
+    };
+  }
+
+  closeSale() {
+    this.setState({
+      showSale: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header-grid-frame">
+          <div className="header-logo"> </div>
+          <div className="header-title">
+            LOVELACE LEGENDS PRODUCT PAGE
+          </div>
+          <div className="header-search-bar">
+            SEARCH ME
+          </div>
+        </div>
+        <SaleModal />
+      </div>
+    );
+  }
+}
 
 export default Header;
---
+++ client/src/components/header/SaleModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SaleModal = (props) => (
+  <div> MODAL </div>
+);
+
+export default SaleModal;
