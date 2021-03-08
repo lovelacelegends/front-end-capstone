a47b634f2d3846aa9979039fe1b718d1deded4ea
Timothy Nguyen <shimoshixp@gmail.com>
2021-03-08 02:10:44 -0800

    Refactor Review class to be moved to RatingsAndReviews

--- client/src/components/ratingsAndReviews/RatingsAndReviews.jsx
+++ client/src/components/ratingsAndReviews/RatingsAndReviews.jsx
@@ -8,15 +8,57 @@ class RatingsAndReviews extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      reviewCount: 2,
+      showModal: false,
+      showMoreReviews: true,
+    };
+
+    this.handleMoreReviewsClick = this.handleMoreReviewsClick.bind(this);
+    this.resetReviewCount = this.resetReviewCount.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  handleMoreReviewsClick() {
+    const { reviews } = this.props;
+    const { reviewCount } = this.state;
+
+    if (reviewCount + 2 >= reviews.results.length) {
+      this.setState((prevState) => ({
+        showMoreReviews: false,
+        reviewCount: prevState.reviewCount + 2,
+      }));
+    } else {
+      this.setState((prevState) => ({
+        reviewCount: prevState.reviewCount + 2,
+      }));
+    }
+  }
+
+  resetReviewCount() {
+    this.setState({
+      reviewCount: 2,
+    });
+  }
+
+  toggleModal() {
+    this.setState((prevState) => ({
+      showModal: !prevState.showModal,
+    }));
   }
 
   render() {
     const {
       selectedProduct,
       reviews,
-      meta
+      meta,
     } = this.props;
+
+    const {
+      reviewCount,
+      showModal,
+      showMoreReviews,
+    } = this.state;
 
     return (
       <div className="ratings-and-reviews">
@@ -25,6 +67,12 @@ class RatingsAndReviews extends React.Component {
           selectedProduct={selectedProduct}
           reviews={reviews}
           meta={meta}
+          reviewCount={reviewCount}
+          showModal={showModal}
+          showMoreReviews={showMoreReviews}
+          handleMoreReviewsClick={this.handleMoreReviewsClick}
+          resetReviewCount={this.resetReviewCount}
+          toggleModal={this.toggleModal}
         />
       </div>
     );
