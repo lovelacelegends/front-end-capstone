6caf2c77d2e267dc827d3a29b3e509ca46deba58
Jack <jackpronske@yahoo.com>
2021-03-10 00:13:56 -0800

    --making some headway on the header!

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { GrSearch } from 'react-icons/gr';
 import SaleModal from './SaleModal';
 
-// const Header = () => (
-//   <div>Header</div>
-// );
-
 class Header extends React.Component {
   constructor(props) {
     super(props);
@@ -24,17 +20,10 @@ class Header extends React.Component {
   render() {
     const { showSale } = this.state;
     return (
-      <div>
-        <div className="header-grid-frame">
-          <div className="header-logo"> </div>
-          <div className="header-title"> </div>
-          <div className="header-search-bar">
-            <div className="fake-search-bar">
-              _____________
-            </div>
-            <GrSearch />
-          </div>
-        </div>
+      <div className="header-grid-frame">
+        <div className="header-logo"> </div>
+        <div className="header-upper-middle"> </div>
+        <div className="header-search-bar"> hell </div>
         {showSale ? <SaleModal closeSale={this.closeSale} /> : null}
       </div>
     );
