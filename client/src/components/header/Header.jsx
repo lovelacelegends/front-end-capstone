f42b85ba47cf388ae226f18cc8e1c428d8359396
Jack <jackpronske@yahoo.com>
2021-03-10 00:45:00 -0800

    implemented a decent header with a search bar that doesn't do anything

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GrSearch } from 'react-icons/gr';
+import HeaderSearchBar from './HeaderSearchBar';
 import SaleModal from './SaleModal';
 
 class Header extends React.Component {
@@ -23,7 +23,7 @@ class Header extends React.Component {
       <div className="header-grid-frame">
         <div className="header-logo"> </div>
         <div className="header-upper-middle"> </div>
-        <div className="header-search-bar"> hell </div>
+        <HeaderSearchBar />
         {showSale ? <SaleModal closeSale={this.closeSale} /> : null}
       </div>
     );
